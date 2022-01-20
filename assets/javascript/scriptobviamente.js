51fc898130c728da6fa10eab44c2abf8210adc28
Jonas Portela <86263451+JonasBPortela@users.noreply.github.com>
2022-01-20 11:58:52 -0400

    Update pagina academicos cadastro
    Incrementado funcao javascript para sidenavbar
    adicionado campos de nome data e inicio de criacao do campo radio
    correcao de pequenos erros com tamanho

--- assets/javascript/scriptobviamente.js
+++ assets/javascript/scriptobviamente.js
@@ -1,7 +1,19 @@
+var fechado = true;
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "316px";
+  if(fechado){
+    document.getElementById("mySidenav").style.width = "17%";
+    document.getElementById("conteudo").style.width = "83%";
+    fechado = false;
   }
-  
+  else{
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("conteudo").style.width = "100%";
+    fechado = true;
+  }
+}
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("conteudo").style.width = "100%";
+    fechado = true;
   }
